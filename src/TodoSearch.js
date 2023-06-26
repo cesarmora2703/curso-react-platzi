9b447938cd69afbba31db6d5d4204a2b52a9b9b2
cesarmora2703 <cesarmora2703@gmail.com>
2023-06-26 10:47:38 -0500

    class_7 states use and update

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,10 +1,18 @@
+import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState('');
+
+    console.log("Se ha buscado: " + searchValue)
+
     return (
         <input
             placeholder="A cada dia su afán."
             className='TodoSearch'
+            onChange={(event) => {
+                setSearchValue(event.target.value);
+            }}
         />
     );
 }
